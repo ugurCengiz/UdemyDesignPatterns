838315b0d6228a3d94393ec040399d8c0ad8e49d
uğur cengiz <ugur_nobre@hotmail.com>
2022-11-04 17:44:39 +0300

    04.11.22 commit 2

--- WebApp.Command/Commands/PdfFile.cs
+++ WebApp.Command/Commands/PdfFile.cs
@@ -1,26 +1,29 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using DinkToPdf;
+﻿using DinkToPdf;
 using DinkToPdf.Contracts;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace WebApp.Command.Commands
 {
     public class PdfFile<T>
     {
-        public readonly List<T> _List;
-        public readonly HttpContext _Context;
-        public string FileName => $"{typeof(T).Name}.pdf";
-        public string FileType => "application/octet-stream";
+        public readonly List<T> _list;
+        public readonly HttpContext _context;
 
         public PdfFile(List<T> list, HttpContext context)
         {
-            _List = list;
-            _Context = context;
+            _list = list;
+            _context = context;
         }
+
+        public string FileName => $"{typeof(T).Name}.pdf";
+        public string FileType => "application/octet-stream";
 
         public MemoryStream Create()
         {
@@ -29,10 +32,10 @@ namespace WebApp.Command.Commands
             var sb = new StringBuilder();
 
             sb.Append($@"<html>
-                        <head></head>
-                        <body>
-                        <div class='text-center'><h1></h1>{type.Name} tablo </div>
-                        <table class='table table-striped'align='center'>");
+                          <head></head>
+                          <body>
+                            <div class='text-center'><h1>{type.Name} tablo</h1></div>
+                            <table class='table table-striped' align='center'>");
 
             sb.Append("<tr>");
             type.GetProperties().ToList().ForEach(x =>
@@ -41,47 +44,42 @@ namespace WebApp.Command.Commands
             });
             sb.Append("</tr>");
 
-            _List.ForEach(x =>
+            _list.ForEach(x =>
             {
-                var values = type.GetProperties().Select(propertyInfo => propertyInfo.GetValue(x, null)).ToList();
+                var values = type.GetProperties().Select(properyInfo => properyInfo.GetValue(x, null)).ToList();
 
                 sb.Append("<tr>");
-                
-                values.ForEach(values =>
+
+                values.ForEach(value =>
                 {
-                    sb.Append($"<td>{values}</td>");
+                    sb.Append($"<td>{value}</td>");
                 });
 
                 sb.Append("</tr>");
-
-
             });
 
-            sb.Append("</table></body></html>");
+            sb.Append("</table></body></html");
 
             var doc = new HtmlToPdfDocument()
             {
                 GlobalSettings = {
-                    ColorMode = ColorMode.Color,
-                    Orientation = Orientation.Portrait,
-                    PaperSize = PaperKind.A4,
-                },
+                        ColorMode = ColorMode.Color,
+                        Orientation = Orientation.Portrait,
+                        PaperSize = PaperKind.A4,
+    },
                 Objects = {
-                    new ObjectSettings() {
+                new ObjectSettings() {
                         PagesCount = true,
-                        HtmlContent = sb.ToString() ,
-                        WebSettings = { DefaultEncoding = "utf-8",UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(),"www.root/lib/bootstrap/dist/css/bootstrap.css")},
+                        HtmlContent = sb.ToString(),
+                        WebSettings = { DefaultEncoding = "utf-8",UserStyleSheet=Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/lib/bootstrap/dist/css/bootstrap.css") },
                         HeaderSettings = { FontSize = 9, Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 }
-                    }
-                }
+        }
+    }
             };
 
-            var converter = _Context.RequestServices.GetRequiredService<IConverter>();
+            var converter = _context.RequestServices.GetRequiredService<IConverter>();
 
-             return new(converter.Convert(doc));
-             
+            return new(converter.Convert(doc));
         }
-
-
     }
 }
